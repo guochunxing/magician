39ecf2f6a475a2a8996b89e7bef260949f5614fb
karl <workabee@126.com>
2021-01-12 17:52:13 +0800

    init

---
+++ magician-auth/src/test/java/com/workabee/cloud/magician/MagicianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workabee.cloud.magician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
